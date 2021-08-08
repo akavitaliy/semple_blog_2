cb899c6daece77ca39fa90dbac397c99227489c2
VitamiN <scoundrel98@gmail.com>
2021-08-08 17:55:44 +0300

    localization form and headers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,7 +19,8 @@ class PostsController < ApplicationController
     if @post.save 
       redirect_to @post, success: 'Статья успешно создана!'
     else
-      render :new, danger: 'Статья не создана!'
+      flash[:danger] = 'Статья не создана!'
+      render :new
     end
   end
 
@@ -31,7 +32,8 @@ class PostsController < ApplicationController
     if @post.update(post_params)
       redirect_to @post, success: 'Стотья успешно обновлена!' 
     else
-      render :edit, danger: 'Статья не обновлена!'
+      flash[:danger] = 'Статья не обнавлена!'
+      render :edit
     end
   end
 
--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,7 @@ module SempleBlog2
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.time_zone = 'Minsk'
+    config.i18n.default_locale = :ru
   end
 end
